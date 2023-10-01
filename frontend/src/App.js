684e7429e12f263802e5ea62d50480f580064c43
Rochelle <rli484@wisc.edu>
2023-09-30 23:25:08 -0500

    Trades

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import Marketplace from "./pages/Marketplace"
 import Home from "./pages/Home"
+import Trade from "./pages/Trade";
 import './App.css';
+import { BaseProvider, LightTheme, DarkTheme } from "baseui";
+import { Provider as StyletronProvider } from 'styletron-react';
+import {withStyle, useStyletron} from 'baseui';
+import { Client as Styletron } from 'styletron-engine-atomic';
 
+const engine = new Styletron();
 function App() {
   return (
-    <>
+    <StyletronProvider value={engine}>
+    <BaseProvider theme={DarkTheme}>
       <Home/>
       <Marketplace/>
-    </>
+      <Trade/>
+    </BaseProvider>
+    </StyletronProvider>
   )
 }
 
